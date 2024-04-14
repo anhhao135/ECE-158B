ab8d543cda7109a7221acaca7ebe231c0234537b
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-04-14 12:50:33 -0700

    changes

--- hw-1/UDPPingerClient.py
+++ hw-1/UDPPingerClient.py
@@ -2,11 +2,28 @@
 # We will need the following module to generate randomized lost packets
 import random
 from socket import *
+import time
 
 serverName = '192.168.1.178'
 serverPort = 1919
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = 'test123'
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+
+for i in range(10):
+	time.sleep(0.1)
+	message = 'test' + str(i)
+	clientSocket.sendto(message.encode(), (serverName, serverPort))
+	clientSocket.settimeout(1)
+
+	try:
+
+		message, address = clientSocket.recvfrom(1024)
+		print("Received messsage: " + message.decode())
+		print("From: " + str(address))
+
+
+	except:
+		print("timed out")
+
 
 
--- hw-1/UDPPingerServer.py
+++ hw-1/UDPPingerServer.py
@@ -24,8 +24,8 @@ while True:
 	# Capitalize the message from the client
 	message = message.upper()
 	# If rand is less is than 4, we consider the packet lost and do not respond
-	if rand < 4:
-		continue
+	#if rand < 4:
+	#	continue
 	# Otherwise, the server responds
 	serverSocket.sendto(message, address)
 
