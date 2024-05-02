30d30b9e859e0c9aed1ed6c5a8e0694eaa65a482
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-02 10:18:13 -0700

    webserver http test

---
+++ hw-2/WebServer_Test.py
@@ -0,0 +1,16 @@
+#ECE158B, HW2. Skeleton Python Code for the Web Server.
+#import socket module
+from socket import *
+import sys # In order to terminate the program
+serverSocket = socket(AF_INET, SOCK_STREAM) #TCP
+serverSocket.bind(('', 1919))
+print("Server is now listening")
+
+
+
+while True:
+    message, address = serverSocket.recvfrom(1024)
+    # Print out datagram info
+    print("Received messsage: " + message.decode())
+    print("From: " + str(address))
+    print("---------------------------------------")
