c1f8120d7e87c93fbeb706d821233d2fb54329fb
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-02 10:22:15 -0700

    Update WebServer_Test.py

--- hw-2/WebServer_Test.py
+++ hw-2/WebServer_Test.py
@@ -4,13 +4,16 @@ from socket import *
 import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM) #TCP
 serverSocket.bind(('', 1919))
+serverSocket.listen(1)
 print("Server is now listening")
 
-
-
 while True:
-    message, address = serverSocket.recvfrom(1024)
+    clientConnection, clientAddress = serverSocket.accept()
+    message, address = clientConnection.recvfrom(1024)
     # Print out datagram info
     print("Received messsage: " + message.decode())
     print("From: " + str(address))
     print("---------------------------------------")
+    clientConnection.close()
+
+serverSocket.close()
