6823ded9c97806fd5b36830919f71a4c71215b22
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-02 10:36:03 -0700

    Update WebServer_Test.py

--- hw-2/WebServer_Test.py
+++ hw-2/WebServer_Test.py
@@ -1,7 +1,20 @@
 #ECE158B, HW2. Skeleton Python Code for the Web Server.
 #import socket module
 from socket import *
-import sys # In order to terminate the program
+import signal
+import sys
+import time
+import threading
+
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    serverSocket.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+print('Press Ctrl+C')
+forever = threading.Event()
+forever.wait()
 
 IP = '192.168.1.178'
 PORT = 8000
@@ -12,16 +25,14 @@ serverSocket.listen(1)
 print("Server is now listening")
 
 while True:
-    try:
-        clientConnection, clientAddress = serverSocket.accept()
-        print("here")
-        message, address = clientConnection.recvfrom(1024)
-        # Print out datagram info
-        print("Received messsage: " + message.decode())
-        print("From: " + str(address))
-        print("---------------------------------------")
-        response = 'HTTP/1.0 200 OK\n\nHello World'
-        clientConnection.sendall(response.encode())
-        clientConnection.close()
-    except KeyboardInterrupt:
-        serverSocket.close()
+
+    clientConnection, clientAddress = serverSocket.accept()
+    print("here")
+    message, address = clientConnection.recvfrom(1024)
+    # Print out datagram info
+    print("Received messsage: " + message.decode())
+    print("From: " + str(address))
+    print("---------------------------------------")
+    response = 'HTTP/1.0 200 OK\n\nHello World'
+    clientConnection.sendall(response.encode())
+    clientConnection.close()
