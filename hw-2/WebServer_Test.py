bc528f1f65651c50b206954b183c5358797d6697
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-02 11:12:19 -0700

    Update WebServer_Test.py

--- hw-2/WebServer_Test.py
+++ hw-2/WebServer_Test.py
@@ -12,14 +12,20 @@ serverSocket.listen(1)
 print("Server is now listening")
 
 while True:
-
-    connectionSocket, clientAddress = serverSocket.accept()
-    message, address = connectionSocket.recvfrom(1024)
-    filename = message.decode().split()[1]
-    f = open(filename[1:], 'rb')
-    response = f.read()
-    f.close()
-    connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-    connectionSocket.send(response)
-    connectionSocket.send("\r\n".encode())
-    connectionSocket.close() 
+    try:
+        connectionSocket, clientAddress = serverSocket.accept()
+        message, address = connectionSocket.recvfrom(1024)
+        filename = message.decode().split()[1]
+        f = open(filename[1:], 'rb')
+        response = f.read()
+        f.close()
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        connectionSocket.send(response)
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close() 
+    except IOError:
+        # Send HTTP response message for file not found
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+        # Close the client connection socket
+        connectionSocket.close()
