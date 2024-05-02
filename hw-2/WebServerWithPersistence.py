99e7cb7eb3cde49f1168df3477ffb5544a984ca2
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-02 14:23:14 -0700

    Update WebServerWithPersistence.py

--- hw-2/WebServerWithPersistence.py
+++ hw-2/WebServerWithPersistence.py
@@ -20,19 +20,39 @@ while True:
         print("From: " + str(clientAddress))
         print("---------------------------------------")
         connectionType = message.decode().split()[6] #either keep-alive or close
+        print(connectionType)
 
-        filename = message.decode().split()[1] #get the first field of the message which is the request file name and directory
-        f = open(filename[1:], 'rb') #open the file by path in the server local directory
-        response = f.read() #read the file contents as bytes and save it in a response variable
-        f.close() #close out file so other processes can access it
-        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode()) #send an OK 200 response as the header so the client's browser can signify a successful page load to the client
-        #leave a blank line with two carriage returns and two new lines
-        #below the blank line are the contents of the HTML page to be rendered on the client's browser
-        connectionSocket.send(response) #send the response to the client
-        connectionSocket.send("\r\n".encode()) #send a carriage return and new line to signify the end of the HTTP response
+        if connectionType == 'keep-alive':
+            while True:
+                print("KEEPING ALIVE")
+                filename = message.decode().split()[1] #get the first field of the message which is the request file name and directory
+                f = open(filename[1:], 'rb') #open the file by path in the server local directory
+                response = f.read() #read the file contents as bytes and save it in a response variable
+                f.close() #close out file so other processes can access it
+                connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode()) #send an OK 200 response as the header so the client's browser can signify a successful page load to the client
+                #leave a blank line with two carriage returns and two new lines
+                #below the blank line are the contents of the HTML page to be rendered on the client's browser
+                connectionSocket.send(response) #send the response to the client
+                connectionSocket.send("\r\n".encode()) #send a carriage return and new line to signify the end of the HTTP response
+                message, address = connectionSocket.recvfrom(1024) #receive a message from the client; this is expected to be an HTTP request
+                #debug print out the request
+                print("---------------------------------------")
+                print("Received messsage: " + message.decode())
+                print("From: " + str(clientAddress))
+                print("---------------------------------------")
+        else:
+                print("ONE TIME")
+                filename = message.decode().split()[1] #get the first field of the message which is the request file name and directory
+                f = open(filename[1:], 'rb') #open the file by path in the server local directory
+                response = f.read() #read the file contents as bytes and save it in a response variable
+                f.close() #close out file so other processes can access it
+                connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode()) #send an OK 200 response as the header so the client's browser can signify a successful page load to the client
+                #leave a blank line with two carriage returns and two new lines
+                #below the blank line are the contents of the HTML page to be rendered on the client's browser
+                connectionSocket.send(response) #send the response to the client
+                connectionSocket.send("\r\n".encode()) #send a carriage return and new line to signify the 
 
-        if connectionType == 'close':
-            connectionSocket.close() #close the TCP connection so other clients can request
+        connectionSocket.close() #close the TCP connection so other clients can request
 
     except IOError: #if opening the request file path results in the file not existing, catch this error and return a 404 HTTP response as well as a 404 not found HTML page
         # Send HTTP response message for file not found
