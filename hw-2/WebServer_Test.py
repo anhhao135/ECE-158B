7cbea1c0324d095efcb5c7526aa99e83ecf13bff
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-02 11:06:04 -0700

    Update WebServer_Test.py

--- hw-2/WebServer_Test.py
+++ hw-2/WebServer_Test.py
@@ -15,11 +15,10 @@ while True:
 
     connectionSocket, clientAddress = serverSocket.accept()
     message, address = connectionSocket.recvfrom(1024)
-    filename = message.split()[1]
+    filename = message.decode().split()[1]
     f = open(filename[1:])
+    response = f.read()
     connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-    for i in range(0, len(f)):
-            # encode the string using the provided encoding. This function returns the bytes object. If we don’t provide encoding, “utf-8” encoding is used as default.
-            connectionSocket.send(f[i].encode())
+    connectionSocket.send(response)
     connectionSocket.send("\r\n".encode())
     connectionSocket.close() 
