a8efe20016817feaf1ca2796ec10a78ca9f5a0d3
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-02 10:33:56 -0700

    Update WebServer_Test.py

--- hw-2/WebServer_Test.py
+++ hw-2/WebServer_Test.py
@@ -12,15 +12,16 @@ serverSocket.listen(1)
 print("Server is now listening")
 
 while True:
-    clientConnection, clientAddress = serverSocket.accept()
-    print("here")
-    message, address = clientConnection.recvfrom(1024)
-    # Print out datagram info
-    print("Received messsage: " + message.decode())
-    print("From: " + str(address))
-    print("---------------------------------------")
-    response = 'HTTP/1.0 200 OK\n\nHello World'
-    clientConnection.sendall(response.encode())
-    clientConnection.close()
-
-serverSocket.close()
+    try:
+        clientConnection, clientAddress = serverSocket.accept()
+        print("here")
+        message, address = clientConnection.recvfrom(1024)
+        # Print out datagram info
+        print("Received messsage: " + message.decode())
+        print("From: " + str(address))
+        print("---------------------------------------")
+        response = 'HTTP/1.0 200 OK\n\nHello World'
+        clientConnection.sendall(response.encode())
+        clientConnection.close()
+    except KeyboardInterrupt:
+        serverSocket.close()
