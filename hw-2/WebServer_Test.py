8d78c1796814eb192918fc20511a048883e7d8e9
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-02 10:37:48 -0700

    Update WebServer_Test.py

--- hw-2/WebServer_Test.py
+++ hw-2/WebServer_Test.py
@@ -1,25 +1,12 @@
 #ECE158B, HW2. Skeleton Python Code for the Web Server.
 #import socket module
 from socket import *
-import signal
-import sys
-import time
-import threading
-
-def signal_handler(signal, frame):
-    print('You pressed Ctrl+C!')
-    serverSocket.close()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-print('Press Ctrl+C')
-forever = threading.Event()
-forever.wait()
 
 IP = '192.168.1.178'
 PORT = 8000
 
 serverSocket = socket(AF_INET, SOCK_STREAM) #TCP
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serverSocket.bind((IP, PORT))
 serverSocket.listen(1)
 print("Server is now listening")
