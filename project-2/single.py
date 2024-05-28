04ec61cb138c81298cfdff9fbd8ecfce7896cd93
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-28 16:46:06 -0700

    etc

---
+++ project-2/single.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to create a network and run multiple tests.
+For a more complicated test example, see udpbwtest.py.
+"""
+
+from mininet.cli import CLI
+from mininet.log import lg, info
+from mininet.net import Mininet
+from mininet.node import OVSKernelSwitch
+from mininet.topolib import SingleSwitchTopo
+
+def ifconfigTest( net ):
+    "Run ifconfig on all hosts in net."
+    hosts = net.hosts
+    for host in hosts:
+        info( host.cmd( 'ifconfig' ) )
+
+def iPerfPairsTest(net):
+    h1, h4 = net.getNodeByName('h1', 'h4')
+    net.iperf(( h1, h4 ))
+    
+
+
+if __name__ == '__main__':
+    lg.setLogLevel( 'info' )
+    info( "*** Initializing Mininet and kernel modules\n" )
+    OVSKernelSwitch.setup()
+    info( "*** Creating network\n" )
+    network = Mininet( SingleSwitchTopo( k=4 ), switch=OVSKernelSwitch,
+                       waitConnected=True )
+    info( "*** Starting network\n" )
+    network.start()
+    info( "*** Running ping test\n" )
+    network.pingAll()
+    info( "*** Running iPerf pair tests\n" )
+    iPerfPairsTest(network)
+    info( "*** Starting CLI (type 'exit' to exit)\n" )
+    CLI( network )
+    info( "*** Stopping network\n" )
+    network.stop()
