5cae3b53fad24d9700da83b2806a72a32c92e530
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-28 21:18:29 -0700

    Update main.py

--- project-2/main.py
+++ project-2/main.py
@@ -3,20 +3,24 @@ from mininet.net import Mininet
 from mininet.node import OVSKernelSwitch
 from mininet.topo import SingleSwitchTopo, LinearTopo
 from mininet.topolib import TreeTopo
-from mininet.link import TCIntf
+from mininet.link import TCIntf, TCLink
 from mininet.util import custom
 import time
 from fattree import FatTree
 from mininet.cli import CLI
 
 
+DELAY = '1ms'
+BW = 100
+ 
+
 if __name__ == '__main__':
-    #intf = custom(TCIntf, bw=BW, delay=DELAY)
+    intf = custom(TCIntf, bw=BW, delay=DELAY)
     lg.setLogLevel( 'info' )
     OVSKernelSwitch.setup()
 
     #network = Mininet(FatTree, switch=OVSKernelSwitch, waitConnected=True, intf=intf)
-    network = Mininet(FatTree(), switch=OVSKernelSwitch, waitConnected=True)
+    network = Mininet(FatTree(), switch=OVSKernelSwitch, waitConnected=True, intf=intf)
 
     network.start()
     #network.pingAll()
