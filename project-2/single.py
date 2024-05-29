4e256b78374b2b826b70b4ce0e11e4003335ea8e
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-28 18:34:03 -0700

    Update single.py

--- project-2/single.py
+++ project-2/single.py
@@ -36,7 +36,7 @@ def iPerfSimultaneousTest(net):
         info(host.cmd('iperf -s &'))
     time.sleep(1)
     h1, h2, h3, h4 = net.getNodeByName('h1', 'h2', 'h3', 'h4')
-    info(h1.sendCmd('iperf -c 10.0.0.2 > log12.txt & iperf -c 10.0.0.3 > log13.txt &iperf -c 10.0.0.4 > log14.txt &'))
+    info(h1.cmd('iperf -c 10.0.0.2 > log12.txt & iperf -c 10.0.0.3 > log13.txt &iperf -c 10.0.0.4 > log14.txt &'))
 
     
 
